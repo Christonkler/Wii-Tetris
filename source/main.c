bc4df597f8df1a8509d008dd341d739b1e3bffe5
Christonkler <cjonesworth@gmail.com>
2024-04-19 23:00:29 -0500

    Move T piece with d pad after startup

--- source/main.c
+++ source/main.c
@@ -11,7 +11,6 @@
 
 static u32 *xfb; // SCREEN BOUNDS ARE 240x320
 static GXRModeObj *rmode;
-static u32 backgroundColor;
 //WPAD_BUTTON_2=0x0001
 //WPAD_BUTTON_1=0x0002
 //WPAD_BUTTON_B=0x0004
@@ -47,57 +46,26 @@ void initializeGraphics() {
 
 
 void drawSquare(int startX, int startY, int squareSize, u32 color) {
-	for (int y = startY; y < startY + squareSize; y++) {
+	for (int y = startY; y < startY + 2*squareSize; y++) {
 		for (int x = startX; x < startX + squareSize; x++) {
 			// Calculate the framebuffer index for this pixel
-			int index = y * rmode->fbWidth + x;
+			int index = (y * rmode->fbWidth)/2 + x;
 
 			// Set the pixel color (red in this example)
 			xfb[index] = color; // RGB value for red
 		}
 	}
-	
 }
 
 
-//void drawSquare() {
-//    GXColor color = {255, 0, 0, 255}; // Red color (adjust as needed)
-//
-//    // Initialize GX
-//    GX_Init(&xfb[0], rmode->fbWidth);
-//    GX_SetCopyClear(color, 0xFFFFFF); // Set clear color
-//
-//    // Set up projection and view matrices (you can customize these)
-//    // ...
-//
-//    // Set up vertex data for a quad (square)
-//    guVector v0 = {0.0f, 0.0f, 0.0f}; // Top-left corner
-//    guVector v1 = {50.0f, 0.0f, 0.0f}; // Top-right corner
-//    guVector v2 = {50.0f, 50.0f, 0.0f}; // Bottom-right corner
-//    guVector v3 = {0.0f, 50.0f, 0.0f}; // Bottom-left corner
-//
-//    // Draw the quad
-//    GX_Begin(GX_QUADS, GX_VTXFMT0, 4);
-//    GX_Position3f32(v0.x, v0.y, v0.z);
-//    GX_Color1u32(0xFF0000FF);
-//    GX_Position3f32(v1.x, v1.y, v1.z);
-//    GX_Color1u32(0xFF0000FF);
-//    GX_Position3f32(v2.x, v2.y, v2.z);
-//    GX_Color1u32(0xFF0000FF);
-//    GX_Position3f32(v3.x, v3.y, v3.z);
-//    GX_Color1u32(0xFF0000FF);
-//    GX_End();
-//
-//    // Flush GX commands
-//    GX_DrawDone();
-//
-//    // Clean up
-//    GX_AbortFrame();
-//    GX_Flush();
-//	VIDEO_WaitVSync();
-//    VIDEO_Flush();
-//	VIDEO_WaitVSync();
-//}
+void eraseSquare(int startX, int startY, int squareSize) {
+	for (int y = startY; y < startY + 2*squareSize; y++) {
+		for (int x = startX; x < startX + squareSize; x++) {
+			int index = (y * rmode->fbWidth)/2 + x;
+			xfb[index] = BACKGROUND_COLOR;
+		}
+	}
+}
 
 
 
@@ -135,12 +103,16 @@ void initializeTetrimino(Tetrimino* tetrimino) {
 		
 		case 'T':
 			tetrimino -> color = T_COLOR;
-			tetrimino->tiles[0] = (Tile){3, 1, 0, tetrimino->velocity, T_COLOR};
-			tetrimino->tiles[1] = (Tile){4, 1, 0, tetrimino->velocity, T_COLOR};
-			tetrimino->tiles[2] = (Tile){4, 0, 0, tetrimino->velocity, T_COLOR};
-			tetrimino->tiles[3] = (Tile){5, 1, 0, tetrimino->velocity, T_COLOR};
-			tetrimino->xPosition = 3;
-			tetrimino->yPosition = 1;
+			tetrimino->tiles[0] = (Tile){50, 70, 0, tetrimino->velocity, T_COLOR};
+			drawSquare(tetrimino->tiles[0].xPosition, tetrimino->tiles[0].yPosition, TILE_SIZE, tetrimino->tiles[0].color);
+			tetrimino->tiles[1] = (Tile){60, 70, 0, tetrimino->velocity, T_COLOR};
+			drawSquare(tetrimino->tiles[1].xPosition, tetrimino->tiles[1].yPosition, TILE_SIZE, tetrimino->tiles[1].color);
+			tetrimino->tiles[2] = (Tile){60, 50, 0, tetrimino->velocity, T_COLOR};
+			drawSquare(tetrimino->tiles[2].xPosition, tetrimino->tiles[2].yPosition, TILE_SIZE, tetrimino->tiles[2].color);
+			tetrimino->tiles[3] = (Tile){70, 70, 0, tetrimino->velocity, T_COLOR};
+			drawSquare(tetrimino->tiles[3].xPosition, tetrimino->tiles[3].yPosition, TILE_SIZE, tetrimino->tiles[3].color);
+			tetrimino->xPosition = 50;
+			tetrimino->yPosition = 20;
 			break;
 		
 		case 'S':
@@ -181,8 +153,11 @@ void initializeTetrimino(Tetrimino* tetrimino) {
 
 
 void moveTile(Tile* tile, int xPositionChange, int yPositionChange) {
+	//CHECK IF MOVE IS LEGAL
+	//eraseSquare(tile->xPosition, tile->yPosition, TILE_SIZE);
 	tile->xPosition += xPositionChange;
 	tile->yPosition += yPositionChange;
+	drawSquare(tile->xPosition, tile->yPosition, TILE_SIZE, tile->color);
 }
 
 
@@ -194,24 +169,38 @@ void moveTetriminoButtonPress(Tetrimino* tetrimino, u16 buttonsDown) {
 	
 	if (buttonsDown & WPAD_BUTTON_A) {
 		printf("Button A pressed.\n");
-	} else if (buttonsDown & WPAD_BUTTON_UP) { 
+	} else if (buttonsDown & WPAD_BUTTON_UP) {  // LEFT
 		for (int i = 0; i < 4; i++) {
-			moveTile(&tetrimino->tiles[i], -1, 0);
+			eraseSquare(tetrimino->tiles[i].xPosition, tetrimino->tiles[i].yPosition, TILE_SIZE);
 		}
-		tetrimino->xPosition--;
-	} else if (buttonsDown & WPAD_BUTTON_DOWN) {
 		for (int i = 0; i < 4; i++) {
-			moveTile(&tetrimino->tiles[i], 1, 0);
+			moveTile(&tetrimino->tiles[i], -1*TILE_SIZE, 0);
 		}
-		tetrimino->xPosition++;
-	} else if (buttonsDown & WPAD_BUTTON_RIGHT) {
+		tetrimino->xPosition -= TILE_SIZE;
+	} else if (buttonsDown & WPAD_BUTTON_DOWN) { // RIGHT
 		for (int i = 0; i < 4; i++) {
-			moveTile(&tetrimino->tiles[i], 0, 1);
+			eraseSquare(tetrimino->tiles[i].xPosition, tetrimino->tiles[i].yPosition, TILE_SIZE);
 		}
-	} else if (buttonsDown & WPAD_BUTTON_LEFT) {
 		for (int i = 0; i < 4; i++) {
-			moveTile(&tetrimino->tiles[i], 0, -1);
+			moveTile(&tetrimino->tiles[i], TILE_SIZE, 0);
 		}
+		tetrimino->xPosition += TILE_SIZE;
+	} else if (buttonsDown & WPAD_BUTTON_RIGHT) { // UP
+		for (int i = 0; i < 4; i++) {
+			eraseSquare(tetrimino->tiles[i].xPosition, tetrimino->tiles[i].yPosition, TILE_SIZE);
+		}
+		for (int i = 0; i < 4; i++) {
+			moveTile(&tetrimino->tiles[i], 0, -1*TILE_SIZE);
+		}
+		tetrimino->yPosition -= TILE_SIZE;
+	} else if (buttonsDown & WPAD_BUTTON_LEFT) { // DOWN
+		for (int i = 0; i < 4; i++) {
+			eraseSquare(tetrimino->tiles[i].xPosition, tetrimino->tiles[i].yPosition, TILE_SIZE);
+		}
+		for (int i = 0; i < 4; i++) {
+			moveTile(&tetrimino->tiles[i], 0, TILE_SIZE);
+		}
+		tetrimino->yPosition += TILE_SIZE;
 	}
 }
 
@@ -221,14 +210,26 @@ void movePieceGravity() {
 }
 
 
+ //
+ //
+ //
+ //
+ //    TESTS
+ //
+ //
+ //
+ //
+ //
+ 
+
 int tetriminoMovesLeftOnUpPressTest() {
 	Tetrimino tetrimino;
 	tetrimino.shape = 'T';
 	initializeTetrimino(&tetrimino);
 	moveTetriminoButtonPress(&tetrimino, 0x0800);
 	
-	if (tetrimino.xPosition != 2) {
-		printf("Tetrimino did not move left. Expected position -2 but was %d\n", tetrimino.xPosition);
+	if (tetrimino.xPosition != (50-TILE_SIZE)) {
+		printf("Tetrimino did not move left. Expected position %d but was %d\n", 50-TILE_SIZE, tetrimino.xPosition);
 		return 1;
 	}
 	
@@ -242,8 +243,8 @@ int tetriminoMovesRightOnDownPressTest() {
 	initializeTetrimino(&tetrimino);
 	moveTetriminoButtonPress(&tetrimino, 0x0400);
 	
-	if (tetrimino.xPosition != 4) {
-		printf("Tetrimino did not move right. Expected position 0 but was %d\n", tetrimino.xPosition);
+	if (tetrimino.xPosition != (50+TILE_SIZE)) {
+		printf("Tetrimino did not move right. Expected position %d but was %d\n", 50+TILE_SIZE, tetrimino.xPosition);
 		return 1;
 	}
 	
@@ -259,6 +260,11 @@ int run_tests() {
 }
 
 
+
+
+
+
+
 int main() {
 	initializeGraphics();
 	
@@ -272,21 +278,21 @@ int main() {
 		printf("Frame buffer width: %d\n", rmode->fbWidth);
 	}
 	
-	backgroundColor = xfb[100 * rmode->fbWidth + 100];
 	
 	Tetrimino tetrimino;
 	tetrimino.shape = 'T';
 	initializeTetrimino(&tetrimino);
 	
-	drawSquare(10, 0, 5, J_COLOR); // DARK GREEN
-	drawSquare(15, 0, 5, I_COLOR); // LIGHT BLUE
-	drawSquare(20, 0, 5, 0xFFFFFF88); // WHITE
-	drawSquare(30, 0, 5, S_COLOR); // BRIGHT GREEN
-	drawSquare(35, 0, 5, L_COLOR); // ORANGE
-	drawSquare(45, 0, 5, T_COLOR); // PURPLE
-	drawSquare(50, 0, 5, Z_COLOR); // RED
-	//drawSquare(55, 0, 5, 0x10801080);
-	drawSquare(65, 0, 5, O_COLOR); // YELLOW
+	drawSquare(10, 0, TILE_SIZE, J_COLOR); // DARK GREEN
+	drawSquare(15, 0, TILE_SIZE, I_COLOR); // LIGHT BLUE
+	drawSquare(20, 0, TILE_SIZE, 0xFFFFFF88); // WHITE
+	drawSquare(30, 0, TILE_SIZE, S_COLOR); // BRIGHT GREEN
+	drawSquare(35, 0, TILE_SIZE, L_COLOR); // ORANGE
+	drawSquare(45, 0, TILE_SIZE, T_COLOR); // PURPLE
+	drawSquare(50, 0, TILE_SIZE, Z_COLOR); // RED
+	//drawSquare(55, 0, TILE_SIZE, 0x10801080);
+	drawSquare(65, 0, TILE_SIZE, O_COLOR); // YELLOW
+	printf("ALEX IS A GOOBER");
 	while(1) {
 		WPAD_ScanPads();
 		u16 buttonsDown = WPAD_ButtonsDown(0);
